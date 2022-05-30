3408df3305ab547ead394caf7787ccf326c39fe3
Omar Daoud <omardaoud.dev@gmail.com>
2022-05-30 17:52:01 -0400

    Demo

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = { //exporting an object
+
+    'url' : 'mongodb+srv://oman:demoday@cluster0.pgjsbgh.mongodb.net/demoday?retryWrites=true&w=majority', 
+    'dbName': 'posts'
+};
